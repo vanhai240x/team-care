59c4a282dfe84ab7701a859bb753484f07ba9a2e
vanhai240x <tranvanhai240x@rionlab.com>
2020-03-22 22:10:37 +0700

    change camera ui inprogress

--- js/footer.js
+++ js/footer.js
@@ -1,5 +1,35 @@
 // Footer
 $(document).ready(() => {
+    // Active Link Navigation
+    let navItem = $('.navbar-bottom .link-light');
+    let pathname = window.location.pathname;
+    switch (pathname) {
+        case '/':
+            navItem[0].classList.add('active');
+            break;
+        case '/thay-man-hinh/':
+            navItem[1].classList.add('active');
+            break;
+        case '/thay-pin/':
+            navItem[2].classList.add('active');
+            break;
+        case '/thay-camera/':
+            navItem[3].classList.add('active');
+            break;
+        case '/thay-vo/':
+            navItem[4].classList.add('active');
+            break;
+        case '/tin-tuc/':
+            navItem[5].classList.add('active');
+            break;
+        case '/lien-he/':
+            navItem[6].classList.add('active');
+            break;
+        default:
+            break;
+    }
+
+    // Handle Hover Social Icons
     let iconSocialFooter = $(".footer .line-3 .icon");
     for (let i = 0; i < iconSocialFooter.length; i++) {
         iconSocialFooter[i].addEventListener("mouseover", () => {
